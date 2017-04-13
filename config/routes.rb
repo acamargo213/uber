b28cf4fa048f2f54f8a3e7f80def17a71a35b6d3
Alejandra Camargo <acam.2134@gmail.com>
2017-04-13 22:25:45 +0000

    added login system

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  root to: 'home#index'
+  
+  get '/driver', to: 'driver#status'
+  post '/driver', to: 'driver#update_status'
+  
+  get '/rider', to:'rider#query'
+  post '/rider', to: 'rider#save_ride'
 end
